cdbfd9f5c97cdfb49c4c4261acb6b46f267db943
TristanD <t.r.daams@triznet.com>
2020-09-24 18:31:19 +0200

    Test. Sjaak is Jan geworden.

--- src/nl/novi/javaprogrammeren/Main.java
+++ src/nl/novi/javaprogrammeren/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Wereld!");
 
-        Person p = new Person("Sjaak");
+        Person p = new Person("Jan");
 
         System.out.println(p.getName());
     }
